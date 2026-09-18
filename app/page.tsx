059328acf416a868e5540907bf931dae3d71e323
mannerszeus <mannerszeus@gmail.com>
2026-09-18 17:43:13 +0900

    fix: remove Next.js build dependency

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <iframe
-      src="/dashboard.html"
-      title="Build Monitor"
-      style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
-    />
-  );
-}
